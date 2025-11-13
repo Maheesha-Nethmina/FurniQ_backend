9f61dff91d9935407b22448a3cf8c7f01c3b289c
V.A.K.M.Nethmina <cst21022@std.uwu.ac.lk>
2025-11-13 23:41:25 +0530

    updated

--- src/main/java/com/project/FurniQ/repository/furnitureRepository.java
+++ src/main/java/com/project/FurniQ/repository/furnitureRepository.java
@@ -2,6 +2,10 @@ package com.project.FurniQ.repository;
 
 import com.project.FurniQ.entity.Furniture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
 public interface furnitureRepository extends JpaRepository<Furniture, Integer> {
+    Optional<Furniture> findById(Integer id);
+    List<Furniture> findByName(String name);
 }
--- src/main/java/com/project/FurniQ/service/FurnitureService.java
+++ src/main/java/com/project/FurniQ/service/FurnitureService.java
@@ -1,4 +1,26 @@
 package com.project.FurniQ.service;
 
+import com.project.FurniQ.dto.FurnitureDTO;
+import com.project.FurniQ.entity.Furniture;
+import com.project.FurniQ.repository.furnitureRepository;
+import com.project.FurniQ.util.VarList;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class FurnitureService {
+
+    private final furnitureRepository furnitureRepository;
+    private final ModelMapper modelMapper;
+
+
+    //save furniture details
+    public String saveNewfurniture(FurnitureDTO furnitureDTO){
+        furnitureRepository.save(modelMapper.map(furnitureDTO, Furniture.class));
+        return VarList.RSP_SUCCESS;
+    }
 }
